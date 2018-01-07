03e096315e8c57e6046e500300e8ace48f134437
Evgeni Kolev <evgenysw@gmail.com>
2018-01-07 18:02:49 +0200

    Update publish_to_ghpages.sh

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -13,8 +13,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,5 +22,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing (publish.sh)"
