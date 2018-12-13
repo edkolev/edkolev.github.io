a102801cf5e2a337a57ab587ce67422d88562e02
Evgeni Kolev <evgenysw@gmail.com>
2018-12-13 16:42:46 +0200

    Don't use submodule to publish

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -9,6 +9,15 @@ if [[ $(git status -s) ]]; then
     exit 1;
 fi
 
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
+
 echo "Removing existing files"
 rm -rf public/*
 
