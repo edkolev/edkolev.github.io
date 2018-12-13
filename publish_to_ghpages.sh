ef4f15c44843e000b51ffb2788a2d2538ad01b7b
Evgeni Kolev <evgeni.kolev@uber.com>
2018-12-13 16:29:02 +0200

    Use git submodule to manage publishing

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -9,15 +9,6 @@ if [[ $(git status -s) ]]; then
     exit 1;
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out master branch into public"
-git worktree add -B master public origin/master
-
 echo "Removing existing files"
 rm -rf public/*
 
@@ -28,7 +19,7 @@ echo 'evgeni.io' > public/CNAME
 echo "Updating master branch"
 cd public
 git add --all
-git commit -m "Publishing (publish.sh)"
+git commit -m "Publishing `date`"
 
 echo "Pushing master branch"
-git push
+git push origin master
